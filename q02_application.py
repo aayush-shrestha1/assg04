374f67cf9b2ab4cba5f11207465ba47fa02ebaf2
aayush-shrestha1 <ashrestha56@leomail.etamu.edu>
2026-09-24 14:00:42 -0500

    Implement main application logic for word comparison

--- q02_application.py
+++ q02_application.py
@@ -1,5 +1,45 @@
-"""Question 02 Application Submission
-"""
+from q02 import load_word_set, compare_word_sets
 
-# Put your code to create an application using the question 02
-# functions here
+
+def main():
+    while True:
+        left_filename = input("Enter file name to use for left set of words: ")
+
+        try:
+            left_set = load_word_set(left_filename)
+            break
+        except FileNotFoundError:
+            print("Invalid file name given, try again")
+
+    while True:
+        right_filename = input("Enter file name to use for right set of words: ")
+
+        try:
+            right_set = load_word_set(right_filename)
+            break
+        except FileNotFoundError:
+            print("Invalid file name given, try again")
+
+    while True:
+        comparison = input(
+            "Enter comparison (BOTH, EITHER, LEFT, RIGHT) or QUIT: "
+        )
+
+        if comparison == "QUIT":
+            break
+
+        try:
+            result = compare_word_sets(
+                left_set,
+                right_set,
+                comparison
+            )
+
+            print(sorted(result))
+
+        except Exception:
+            print("Invalid comparison asked for, try again")
+
+
+if __name__ == "__main__":
+    main()
