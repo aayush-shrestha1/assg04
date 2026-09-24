c0a3b83f9337d9730d608f3b9bb3259c7fb48eb7
aayush-shrestha1 <ashrestha56@leomail.etamu.edu>
2026-09-24 13:59:13 -0500

    Add functions to load and compare word sets
    Implemented functions to load a set of words from a file and compare two word sets based on specified criteria.

--- q02.py
+++ q02.py
@@ -1,4 +1,26 @@
-"""Question 02 Submission
-"""
+def load_word_set(filename):
+    words = set()
 
-# Put your code to answer question 02 here
+    with open(filename, "r") as file:
+        for line in file:
+            word = line.strip()
+            words.add(word)
+
+    return words
+
+
+def compare_word_sets(left_set, right_set, comparison):
+    if comparison == "BOTH":
+        return left_set & right_set
+
+    elif comparison == "EITHER":
+        return left_set | right_set
+
+    elif comparison == "LEFT":
+        return left_set - right_set
+
+    elif comparison == "RIGHT":
+        return right_set - left_set
+
+    else:
+        raise Exception(f"Invalid comparison requested: {comparison}")
