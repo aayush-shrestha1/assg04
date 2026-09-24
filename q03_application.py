d782a1e3fc315bcbd620b55d385080d20be545d6
aayush-shrestha1 <ashrestha56@leomail.etamu.edu>
2026-09-24 14:03:31 -0500

    Implement contact management functions with pickle
    Added functions for storing, loading, adding, modifying, and deleting contacts in a database using pickle.

--- q03_application.py
+++ q03_application.py
@@ -1,5 +1,34 @@
-"""Question 03 Application Submission
-"""
+import pickle
 
-# Put your code to create an application using the question 03
-# functions here
+
+def store_database(filename, database):
+    with open(filename, "wb") as file:
+        pickle.dump(database, file)
+
+
+def load_database(filename):
+    with open(filename, "rb") as file:
+        database = pickle.load(file)
+
+    return database
+
+
+def add_contact(database, name, email):
+    if name in database:
+        raise KeyError("Attempt to add new email for existing contact")
+
+    database[name] = email
+
+
+def modify_contact(database, name, email):
+    if name not in database:
+        raise KeyError("Attempt to modify nonexistent contact")
+
+    database[name] = email
+
+
+def delete_contact(database, name):
+    if name not in database:
+        raise KeyError("Attempt to delete nonexistent contact")
+
+    del database[name]
