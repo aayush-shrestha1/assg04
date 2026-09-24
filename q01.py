f11088989a13a8d5aa29f076c70190569a38fa75
aayush-shrestha1 <ashrestha56@leomail.etamu.edu>
2026-09-24 14:07:28 -0500

    Add functions for loading and sorting ranked items
    Implemented functions to load, sort, and retrieve ranked items from a file.

--- q01.py
+++ q01.py
@@ -1,4 +1,29 @@
-"""Question 01 Submission
-"""
+def load_ranked_items(filename):
+    items = []
 
-# Put your code to answer question 01 here
+    with open(filename, "r") as file:
+        while True:
+            rank = file.readline()
+
+            if rank == "":
+                break
+
+            name = file.readline()
+
+            rank = int(rank.strip())
+            name = name.strip()
+
+            items.append((rank, name))
+
+    return items
+
+
+def sort_ranked_items(items):
+    return sorted(items)
+
+
+def get_ranked_items(items, begin, end):
+    if begin < 1 or begin > end or end > len(items):
+        raise Exception("Invalid ranks")
+
+    return items[begin - 1:end]
